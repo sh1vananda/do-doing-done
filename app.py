94918cb16b2b5552b9090da6459a79fa5d9fdb4c
Shivananda Reddy Kankanala <cyberllama13@gmail.com>
2025-06-08 12:54:25 +0530

    Render Deployment Modifications

--- app.py
+++ app.py
@@ -339,4 +339,4 @@ def handle_join_room(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
